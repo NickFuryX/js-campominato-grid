0314ab4e4bac294bbccce972c009becaeb175596
NickfuryX <nosinicolo@gmail.com>
2022-04-05 16:23:41 +0200

    check 4

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 let button = document.getElementById("playButton");
 button.addEventListener("click", gridGenerate);
+button.addEventListener("click", generateBomb);
+let numQuadrati;
+let main = document.getElementById("myMain");
 
 function gridGenerate() {
   let difficolta = document.getElementById("difficolta").value;
-  let main = document.getElementById("myMain");
   main.innerHTML = ""; //(serve a pulire l'html)
 
   let bigSquare = document.createElement("div");
@@ -11,11 +13,12 @@ function gridGenerate() {
 
   if (difficolta === "facile") {
     for (let i = 1; i < 101; i++) {
-      main.appendChild(bigSquare);
+      main.append(bigSquare);
       let littleSquare = document.createElement("div");
       littleSquare.classList.add("mini-quadrato-10x10");
       littleSquare.innerHTML = `${i}`;
       bigSquare.append(littleSquare);
+      littleSquare.addEventListener("click", gameFunction);
     }
   } else if (difficolta === "media") {
     for (let i = 1; i < 82; i++) {
@@ -24,6 +27,7 @@ function gridGenerate() {
       littleSquare.classList.add("mini-quadrato-9x9");
       littleSquare.innerHTML = `${i}`;
       bigSquare.append(littleSquare);
+      littleSquare.addEventListener("click", gameFunction);
     }
   } else {
     for (let i = 1; i < 50; i++) {
@@ -32,6 +36,62 @@ function gridGenerate() {
       littleSquare.classList.add("mini-quadrato-7x7");
       littleSquare.innerHTML = `${i}`;
       bigSquare.append(littleSquare);
+      littleSquare.addEventListener("click", gameFunction);
     }
   }
+}
+
+let attempts = 0;
+let bombArray = [];
+
+function generateBomb() {
+  const BOMB_NUMBER = 16;
+  let bombArray = [];
+  let max_attempt;
+  let difficolta = document.getElementById("difficolta").value;
+  if (difficolta === "facile") {
+    numQuadrati = 100;
+  } else if (difficolta === "media") {
+    numQuadrati = 81;
+  } else {
+    numQuadrati = 49;
+  }
+  max_attempt = numQuadrati - BOMB_NUMBER;
+  while (bombArray.length < BOMB_NUMBER) {
+    let bombNUmber = getRandomInt(1, numQuadrati);
+    if (!bombArray.includes(bombNUmber)) {
+      bombArray.push(bombNUmber);
+    }
+  }
+  console.log(bombArray);
+}
+function gameFunction() {
+  let selectedNumber = parseInt(this.innerText);
+  attempts++;
+  console.log(attempts);
+  if (bombArray.includes(selectedNumber)) {
+    this.style.backgroundColor = "red";
+    gameOver;
+  } else {
+    this.style.backgroundColor = "#6495ed";
+    // gameWin;
+  }
+  this.classList.add("no-pointer");
+  this.removeEventListener("click", gameFunction);
+}
+
+function gameOver() {
+  let loseMessage = document.createElement("div");
+  loseMessage.classList.add("provagameover");
+  loseMessage.innerHTML = `Hai perso in ${attempts} tentativi`;
+  main.append(loseMessage);
+  button.removeEventListener("click", gridGenerate);
+  button.removeEventListener("click", generateBomb);
+}
+
+// Utility
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
 }
